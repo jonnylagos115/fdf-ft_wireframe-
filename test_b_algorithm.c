76d20ec9e2af0263012cc4df6c69afb5564cf59a
Jonathan <jlagos@LAPTOP-0B3F19SJ.localdomain>
2019-08-07 09:49:11 -0700

    done

--- test_b_algorithm.c
+++ test_b_algorithm.c
@@ -11,8 +11,48 @@
 /* ************************************************************************** */
 
 #include "./libft_1/libft.h"
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
 
-int		main(void)
+//Research on the algorithm from PDF
+int		main(int ac, char **ag)
 {
+	if (ac == 5)
+	{
+		unsigned int x1, x2, y1, y2;
+		unsigned int d_x, d_y;
+		float	m, error_b;
+
+		x1 = ft_atoi(ag[1]);
+		x2 = ft_atoi(ag[2]);
+		y1 = ft_atoi(ag[3]);
+		y2 = ft_atoi(ag[4]);
+		d_y = y2 - y1;
+		d_x = x2 - x1;
+		m = (float) d_y / d_x;
+		error_b = m - 1;
+		printf("d_y: %u, d_x: %u\n", d_y, d_x);
+		printf("m: %f\n", m);
+		printf("Illuminate pixel at (%u, %u)\n", x1, y1);
+		if (abs(d_x) >= abs(d_y))
+		{
+			while (x1 < x2 - 1)
+			{
+				x1++;
+				error_b += m;
+				if (error_b > 0)
+				{
+					printf("Illuminate pixel at (%u, %u)\n", x1, y1);
+					y1++;
+					error_b--;
+				}
+				else
+					printf("Illuminate pixel at (%u, %u)\n", x1, y1);
+			}	
+				
+		}
+	}
+	printf("\n");
 	return (0);
 }
